6a04c40ab81d88c703b9d0116ccf48bb02526349
Nguyễn Trọng Hiếu <104880226+tronghieu220403@users.noreply.github.com>
2025-08-12 10:13:10 +0700

    Update def.h

--- Event-Collector-Driver/std/ulti/def.h
+++ Event-Collector-Driver/std/ulti/def.h
@@ -3,18 +3,29 @@ typedef long long ptrdiff_t;
 
 #pragma warning(disable:4100)
 
-template <typename T>
+template <typename ValType>
 struct Less {
 public:
-    inline bool operator()(const T& lhs, const T& rhs) const {
+    inline bool operator()(const ValType& lhs, const ValType& rhs) const {
         return lhs < rhs;
     }
 };
 
-template <typename T>
+template <typename ValType>
 struct Greater {
 public:
-    inline bool operator()(const T& lhs, const T& rhs) const {
+    inline bool operator()(const ValType& lhs, const ValType& rhs) const {
         return lhs > rhs;
     }
 };
+
+template <class T> struct remove_reference { using type = T; };
+template <class T> struct remove_reference<T&> { using type = T; };
+template <class T> struct remove_reference<T&&> { using type = T; };
+template <class T> using remove_reference_t = typename remove_reference<T>::type;
+
+template <class T>
+constexpr remove_reference_t<T>&& Move(T&& t) noexcept {
+	return static_cast<remove_reference_t<T>&&>(t);
+}
+
